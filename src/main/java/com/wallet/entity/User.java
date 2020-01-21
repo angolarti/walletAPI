61a602dce7e24fd93f78f149efa01920f2ac6d63
Walter Angolar da Silva <walter.silva@tistech.co.ao>
2020-01-21 23:09:50 +0100

    finalizado repositorio do usuario

---
+++ src/main/java/com/wallet/entity/User.java
@@ -0,0 +1,58 @@
+package com.wallet.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -6181053560617038719L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
